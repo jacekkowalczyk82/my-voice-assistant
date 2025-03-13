775789a6a111e5b8a96fdaaa1ea2ceaae291862a
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2025-03-13 07:17:06 +0100

    Hello world java 17

---
+++ src/main/java/org/jacekkowalczyk82/tools/voice/App.java
@@ -0,0 +1,13 @@
+package org.jacekkowalczyk82.tools.voice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
